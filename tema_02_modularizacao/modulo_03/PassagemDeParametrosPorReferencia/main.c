a12b573f8e1cd70e4dbe8ba759c5426532524cf0
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-24 22:06:02 -0300

    passing parameters: reference and value

---
+++ tema_02_modularizacao/modulo_03/PassagemDeParametrosPorReferencia/main.c
@@ -0,0 +1,34 @@
+/* libs */
+#include <stdio.h>
+
+/* functions */
+void change(int *x, int *y) {
+	int assistant;
+	
+	// valida enderecos
+	if((x != NULL) && (y != NULL)) { // if((*x != NULL) && (*y != NULL)) {
+		assistant = *x;
+		*x = *y;
+		*y = assistant;
+	}
+}
+	
+/* main method */
+int main(int argc, char *argv[]) {
+	
+	int value_1 = 5;
+	int value_2 = 10;
+	
+	printf("\n---------- Exibibindo dados ---------\n");
+	printf("Antes da troca\n");
+	printf("value_1...: %d\n", value_1);
+	printf("value_2...: %d\n\n", value_2);
+	
+	change(&value_1, &value_2);
+	
+	printf("Depois da troca\n");
+	printf("value_1...: %d\n", value_1);
+	printf("value_2...: %d\n", value_2);
+	
+	exit(0);
+}
