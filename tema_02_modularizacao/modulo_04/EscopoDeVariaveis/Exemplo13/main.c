edc49f1c92f48df5c2fd8fd697805c5293222384
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-25 01:20:35 -0300

    variable scope

---
+++ tema_02_modularizacao/modulo_04/EscopoDeVariaveis/Exemplo13/main.c
@@ -0,0 +1,34 @@
+/* 
+*	library section 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+*	global variable section 
+*/
+
+/*
+*	functions section
+*/
+void functionExample(parameter local variables) {
+	/*
+	*	local variable section
+	*/
+	return;
+}
+
+/*
+*	procedures section
+*/
+
+/* main method */
+int main(int argc, char *argv[]) {
+	
+	/* 
+	*	local variable section
+	*/
+	
+	return 0;
+}
+
